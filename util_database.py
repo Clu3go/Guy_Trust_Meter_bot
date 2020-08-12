d2c76e85bb84034464b4bdcaf4b208f3ab061593
Clu3go <69483924+Clu3go@users.noreply.github.com>
2020-08-12 19:06:46 +0200

    Update util_database.py
    Added first name/last name comparison.

--- util_database.py
+++ util_database.py
@@ -4,17 +4,33 @@ import sqlite3
 
 class DataBase():
 
-	def __init__(self, dbname=""):
+	def __init__(self, dbname="Groups_members.db"):
 		self.dbname = dbname
 		self.conn = sqlite3.connect(dbname)
 		self.c = self.conn.cursor()
 		return
 
+
 	def check_user_id(self, user_id):
-		args = (str(user_id),)
-		stmt = self.c.execute("SELECT CASE WHEN EXISTS (SELECT * FROM '' WHERE user_id=(?)) THEN CAST (1 AS BIT) ELSE CAST(0 AS BIT) END", args)
+		args = (user_id,)
+		stmt = self.c.execute("SELECT CASE WHEN EXISTS (SELECT * FROM 'La_Bibbia_4.0' WHERE user_id=(?)) THEN CAST (1 AS BIT) ELSE CAST(0 AS BIT) END", args)
 		flag = stmt.fetchone()[0]
 		return flag
+
+
+	def check_user_first_name(self, first_name):
+		args = (first_name,)
+		stmt = self.c.execute("SELECT COUNT(*) FROM 'La_Bibbia_4.0' WHERE firstname=(?)", args)
+		flag = stmt.fetchone()[0]
+		return flag
+
+
+	def check_user_last_name(self, last_name):
+		args = (last_name,)
+		stmt = self.c.execute("SELECT COUNT(*) FROM 'La_Bibbia_4.0' WHERE lastname=(?)", args)
+		flag = stmt.fetchone()[0]
+		return flag
+
 
 	def add_feedback(self, feedback_text):
 		args = (feedback_text,)
@@ -24,11 +40,12 @@ class DataBase():
 
 	def check_access_flag(self, user_id):
 		args = (user_id,)
-		stmt = self.c.execute("SELECT IFNULL(, 0) FROM '' WHERE user_id=(?)", args)
+		stmt = self.c.execute("SELECT IFNULL(access_temptative, 0) FROM 'La_Bibbia_4.0' WHERE user_id=(?)", args)
 		return stmt.fetchone()[0]
+
 
 	def add_access_flag(self, user_id):
 		args = (user_id,)
-		self.c.execute("UPDATE '' SET '' = 1 WHERE user_id=(?)", args)
+		self.c.execute("UPDATE 'La_Bibbia_4.0' SET 'access_temptative' = 1 WHERE user_id=(?)", args)
 		self.conn.commit()
 		return
